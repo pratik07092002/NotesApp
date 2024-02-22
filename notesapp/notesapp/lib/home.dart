4ffb28f877baa1c3e1191b9445368d232e54b5d5
pratik07092002 <pratikgaikwad072002@gmail.com>
2024-02-22 21:16:01 +0530

    solved bugs related to updatelist and deletelist ready for API integration

--- notesapp/notesapp/lib/home.dart
+++ notesapp/notesapp/lib/home.dart
@@ -1,4 +1,6 @@
 
+//import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:notesapp/models/notemodel.dart';
 import 'package:notesapp/provider/notesprovider.dart';
@@ -15,9 +17,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late bool isUpdate;
   TextEditingController HeadingController = TextEditingController();
   TextEditingController DescriptionController = TextEditingController();
 void addNote(){
+
     notemod newnote = notemod(
       CreatedOn: DateTime.now(),
        Description: DescriptionController.text.trim().toString(), 
@@ -28,7 +32,8 @@ void addNote(){
         Provider.of<NotesProvider>(context, listen: false).addlist(newnote);
         Navigator.pop(context);
   }
-  void DisplayDialogue(){
+
+void DisplayDialogue(){
     showDialog(context: context
     , builder: (context){
 return  Dialog(child: Container(
@@ -89,24 +94,10 @@ controller: HeadingController,
     });
   }
 
-  void optionsonclick(BuildContext context){
-     showBottomSheet(context: context, builder: (context){
-        return Container(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ListTile(
-                title: Text("option 1"),
-              ),
-              ListTile(
-                title: Text("option 2"),
-              )
-            ],
-          ),
-        );
-      });
-    
-  }
+
+
+
+  
   
   @override
   Widget build(BuildContext context) {
@@ -128,9 +119,98 @@ controller: HeadingController,
                                       mainAxisExtent: 150
                                     ),
        itemBuilder: (context,index){
+        notemod currentnote = notesProvider.notes[index];
         return GestureDetector(
           onLongPress: () {
-            optionsonclick(context);
+            HeadingController.text = currentnote.title!;
+            DescriptionController.text = currentnote.Description!;
+            showBottomSheet(context: context, builder: (context){
+        return Container(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                title: Text("Update"), 
+                onTap: () {
+  notesProvider.updatelist(currentnote);
+
+  showDialog(
+    context: context,
+    builder: (context) {
+      return Dialog(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            color: Colors.black,
+            boxShadow: [
+              BoxShadow(blurStyle: BlurStyle.inner, blurRadius: 5, spreadRadius: 10, color: Colors.grey),
+            ],
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Title(
+                    color: Colors.white,
+                    child: Text("UpdateNote", style: TextStyle(color: Colors.green, fontSize: 20)),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      // Update the title in the currentnote with the new value
+                      currentnote.title = HeadingController.text.trim().toString();
+
+                      // Update the note in the provider
+                      Provider.of<NotesProvider>(context, listen: false).updatelist(currentnote);
+                      
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(Icons.done, color: Colors.green, size: 28),
+                  ),
+                ],
+              ),
+              TextField(
+                controller: HeadingController,
+                cursorColor: Colors.green,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  labelText: "Add title",
+                  labelStyle: TextStyle(color: Colors.white),
+                  focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.green)),
+                  enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.green)),
+                ),
+              ),
+              SizedBox(height: 28,),
+              TextField(
+                controller: DescriptionController,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: "Description",
+                  hintStyle: TextStyle(color: Colors.white),
+                  border: InputBorder.none,
+                ),
+                maxLines: 5,
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+},
+
+              ),
+              ListTile(
+                title: Text("Delete"),
+                onTap: (){
+                  notesProvider.deletelist(currentnote);
+                },
+              )
+            ],
+          ),
+        );
+      });
           },
           child: Container(
             height: 300,
@@ -139,7 +219,7 @@ controller: HeadingController,
               children: [
                 
                 
-                Text(notesProvider.notes[index].title! ,style: TextStyle(fontSize: 28 , color: Colors.white), overflow: TextOverflow.ellipsis,maxLines: 2,),
+                Text(currentnote.title! ,style: TextStyle(fontSize: 28 , color: Colors.white), overflow: TextOverflow.ellipsis,maxLines: 2,),
                 SizedBox(height: 7,),
               
                 
