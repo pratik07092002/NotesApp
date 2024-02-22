77117dce3c8300e382a0b5f81f917bdc0aa45ae5
pratik07092002 <pratikgaikwad072002@gmail.com>
2024-02-22 16:28:42 +0530

    UI design update and delete options implementation

--- notesapp/notesapp/lib/home.dart
+++ notesapp/notesapp/lib/home.dart
@@ -1,6 +1,11 @@
 
 import 'package:flutter/material.dart';
-import 'package:notesapp/newnote.dart';
+import 'package:notesapp/models/notemodel.dart';
+import 'package:notesapp/provider/notesprovider.dart';
+import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
+//import 'package:intl/intl.dart';
+//import 'package:notesapp/newnote.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,27 +15,64 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController HeadingController = TextEditingController();
+  TextEditingController DescriptionController = TextEditingController();
+void addNote(){
+    notemod newnote = notemod(
+      CreatedOn: DateTime.now(),
+       Description: DescriptionController.text.trim().toString(), 
+       id: Uuid().v1() ,
+        name: "Pratik" ,
+         title: HeadingController.text.trim().toString() , 
+         userId: "Pratik.pdg7@gmail.com" );
+        Provider.of<NotesProvider>(context, listen: false).addlist(newnote);
+        Navigator.pop(context);
+  }
   void DisplayDialogue(){
     showDialog(context: context
     , builder: (context){
 return  Dialog(child: Container(
   
-  margin: EdgeInsets.only(left: 10, top: 20, right: 10, bottom: 20),
+  
+  padding: EdgeInsets.all(10),
   decoration: BoxDecoration(
-    color: Colors.red, 
-    
+    borderRadius: BorderRadius.all(Radius.circular(10)),
+    color: Colors.black, 
+    boxShadow: [
+      BoxShadow(blurStyle: BlurStyle.inner , blurRadius: 5, spreadRadius: 10 , color: Colors.grey)
+    ]
     ),
-  child: Column(children: [
+  child: Column(
     
+    children: [
+    Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      
+      children: [
+      Title(color: Colors.white, child: Text("Add Note" , style: TextStyle(color: Colors.green,fontSize: 25),)),
+      SizedBox(width: 125),
+      IconButton(onPressed: (){
+        addNote();
+      }, icon: Icon(Icons.done , color: Colors.green,size: 28,))
+    ],),
     TextField(
+controller: HeadingController,
+      cursorColor: Colors.green,
+      style: TextStyle(color: Colors.white),
       decoration: InputDecoration(
         labelText: "Add title",
-        labelStyle: TextStyle(color: Colors.white)
+        labelStyle: TextStyle(color: Colors.white) ,
+        focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.green)),
+        enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.green)),
+        
+        
         
       ),
     ),
     SizedBox(height: 28,),
     TextField(
+      controller: DescriptionController,
+      style: TextStyle(color: Colors.white),
       decoration: InputDecoration(
         hintText: "Description",
         hintStyle: TextStyle(color: Colors.white),
@@ -39,39 +81,81 @@ return  Dialog(child: Container(
         
       ),
       maxLines: 5,
+      
     ),
+
 
   ],),),);
     });
   }
+
+  void optionsonclick(BuildContext context){
+     showBottomSheet(context: context, builder: (context){
+        return Container(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                title: Text("option 1"),
+              ),
+              ListTile(
+                title: Text("option 2"),
+              )
+            ],
+          ),
+        );
+      });
+    
+  }
+  
   @override
   Widget build(BuildContext context) {
+  NotesProvider notesProvider =  Provider.of<NotesProvider>(context);
     return  Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black45,
         title: const Row(
         children: [
-          Text("Notes",style: TextStyle(color: Colors.white),),
+          Text("Notes",style: TextStyle(color: Colors.blueGrey),),
           Text("App",style: TextStyle(color: Colors.green),)
         ],
       ),),
       body: SafeArea(child: GridView.builder(
+
                                     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                                      crossAxisCount: 2
+                                      crossAxisCount: 2,
+                                      mainAxisExtent: 150
                                     ),
        itemBuilder: (context,index){
-        return Container(
-          margin: const EdgeInsets.all(10),
-          color: Colors.amber,
+        return GestureDetector(
+          onLongPress: () {
+            optionsonclick(context);
+          },
+          child: Container(
+            height: 300,
+            
+            child: Column(
+              children: [
+                
+                
+                Text(notesProvider.notes[index].title! ,style: TextStyle(fontSize: 28 , color: Colors.white), overflow: TextOverflow.ellipsis,maxLines: 2,),
+                SizedBox(height: 7,),
+              
+                
+              ],
+            ),
+            margin: const EdgeInsets.all(10),
+            color: Colors.blueGrey,
+          ),
         );
        },
-       itemCount : 5
+       itemCount : notesProvider.notes.length
        
        )),
       floatingActionButton: FloatingActionButton(onPressed: (){
         DisplayDialogue();
-      }),
+      }, child: Icon(Icons.add),),
     );
   }
 }
